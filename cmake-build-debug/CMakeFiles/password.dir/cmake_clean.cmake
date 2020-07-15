84356be0bc48b6c1e6a6387de0305bc09bdf3dd7
Ricardo De la Vega <ricardo@spacetime.mx>
2020-07-15 01:12:20 -0500

    CMake

---
+++ cmake-build-debug/CMakeFiles/password.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/password.dir/main.c.o"
+  "password"
+  "password.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/password.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
